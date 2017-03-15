c880448645dc706f0690dd03dff5e16fc8f7f7ed
Shawn <shawngaocn@gmail.com>
2017-03-16 01:58:47 +0800

    update xg - wrap gin handler

--- xg/xg.go
+++ xg/xg.go
@@ -12,56 +12,56 @@ type HandleString func(c *gin.Context) (code int, str string)
 type HandleData func(c *gin.Context) (code int, contentType string, data []byte)
 type HandleFile func(c *gin.Context) (filepath string)
 
-func HTML(fn GinHTML) gin.HandlerFunc {
+func HTML(fn HandleHTML) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		code, name, obj := fn(c)
 		c.HTML(code, name, obj)
 	}
 }
 
-func JSON(fn GinObject) gin.HandlerFunc {
+func JSON(fn HandleObject) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		code, obj := fn(c)
 		c.JSON(code, obj)
 	}
 }
 
-func XML(fn GinObject) gin.HandlerFunc {
+func XML(fn HandleObject) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		code, obj := fn(c)
 		c.XML(code, obj)
 	}
 }
 
-func YAML(fn GinObject) gin.HandlerFunc {
+func YAML(fn HandleObject) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		code, obj := fn(c)
 		c.YAML(code, obj)
 	}
 }
 
-func String(fn GinString) gin.HandlerFunc {
+func String(fn HandleString) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		code, obj := fn(c)
 		c.String(code, obj)
 	}
 }
 
-func Redirect(fn GinString) gin.HandlerFunc {
+func Redirect(fn HandleString) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		code, obj := fn(c)
 		c.Redirect(code, obj)
 	}
 }
 
-func Data(fn GinData) gin.HandlerFunc {
+func Data(fn HandleData) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		code, contentType, data := fn(c)
 		c.Data(code, contentType, data)
 	}
 }
 
-func File(fn GinFile) gin.HandlerFunc {
+func File(fn HandleFile) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		http.ServeFile(c.Writer, c.Request, fn(c))
 	}
